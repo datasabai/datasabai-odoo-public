715bb03a02ac21c483abeaf3dfafc1ede468e933
x-jiali <jia.li@datasabai.com>
2026-01-16 11:45:22 +0900

    change template name

--- ds_account_invoice_th/__manifest__.py
+++ ds_account_invoice_th/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Thailand Invoice',
-    'version': '19.0.1.2',
+    'version': '19.0.1.3',
     'summary': "Customizations for Compliance with Thai Invoice Regulations",
     'description': """
 This module customizes the standard module to ensure compliance with Thai invoice regulations.
